45c447ca1a62d5e2fc0fc59fb544b8d04aa83ddb
offfahad <mughalfahad544@gmail.com>
2024-02-29 19:52:22 +0500

    first-commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pymongo
+
+uri = "mongodb://127.0.0.1:27017"
+client = pymongo.MongoClient(uri)
+database = client['fullstack']
+collections = database['students']
+
+students = collections.find({})
+
+for student in students:
+    print(student)
